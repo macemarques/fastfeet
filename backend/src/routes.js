5cbda965ba074cce775c4d322945fff5cac64f4c
macemarques <marcelomarques2007@gmail.com>
2020-01-30 09:59:10 -0300

    CHORE: authMiddleware created.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
-
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/users', authMiddleware, UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
