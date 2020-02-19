93914225838e8d7c3ddacac78e5d8c95e74326e0
macemarques <marcelomarques2007@gmail.com>
2020-02-19 06:18:34 -0300

    CHORE: Recipient search added using query parameters

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,7 +1,50 @@
 import * as Yup from 'yup';
+import { Op } from 'sequelize';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
+  async index(req, res) {
+    const { recipientSearch } = req.query;
+
+    if (recipientSearch) {
+      const recipients = await Recipient.findAll({
+        attributes: [
+          'id',
+          'name',
+          'zipcode',
+          'state',
+          'city',
+          'street',
+          'number',
+          'addicional_address_info',
+        ],
+        order: [['name', 'ASC']],
+        where: { name: { [Op.iLike]: `%${recipientSearch}%` } },
+      });
+
+      if (recipients.length === 0) {
+        return res.status(400).json({ error: 'Nothing found...' });
+      }
+
+      return res.status(200).json(recipients);
+    }
+
+    const recipientList = await Recipient.findAll({
+      attributes: [
+        'id',
+        'name',
+        'zipcode',
+        'state',
+        'city',
+        'street',
+        'number',
+        'addicional_address_info',
+      ],
+    });
+
+    return res.status(200).json(recipientList);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string()
